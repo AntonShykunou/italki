c72e604d7030af4d70d7f3cef8b45a2a429de3e0
Artsiom-Shlapakou <xpressmusik@mail.ru>
2019-11-30 23:41:48 +0300

    lessonmodel commit

--- backend/lessons/models.py
+++ backend/lessons/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Lesson(models.Model):
-    pass
+    teacher = models.ForeignKey(teacher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    price = models.FloatField()
+    description = models.CharField(max_length=20)
+    count_lessons = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
