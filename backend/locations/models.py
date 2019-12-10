5ca502e8705e0cefe038c61072f2a2e22699187d
Artsiom-Shlapakou <xpressmusik@mail.ru>
2019-12-10 15:58:04 +0300

    update_model

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -13,10 +13,11 @@ class Country(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=100)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
-        return "{} city, {}".format(self.name, self.country.name)
+        return "{0}, {1}".format(self.name, self.country.name)
     
     class Meta:
         varbose_name_plural = 'Cities'
