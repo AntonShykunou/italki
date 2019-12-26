b9b9a17d51b6d59737a9b396a4157a23dfc6931c
Artsiom-Shlapakou <xpressmusik@mail.ru>
2019-12-26 21:21:24 +0300

    requirements commit

--- backend/locations/management/commands/load_city.py
+++ backend/locations/management/commands/load_city.py
@@ -1,5 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
 from locations.models import City
+from zipfile import ZipFile
+from itertools import groupby
+import requests, json
+
+def txt_to_json(file):
+    cities = []
+    with open(file, encoding='utf-8') as file:
+    # open('cities.json', 'w') as out_file:
+        for line in in_file:
+            line = line.split('	')
+            cities.append({'city': line[1]})
+    return json.dumps({'cities': cities})
+
+
 
 class Command(BaseCommand):
     help = 'Load cities in db'
@@ -8,13 +22,8 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        pass
-    #     for poll_id in options['poll_ids']:
-    #         try:
-    #             poll = Poll.objects.get(pk=poll_id)
-    #         except Poll.DoesNotExist:
-    #             raise CommandError('Poll "%s" does not exist' % poll_id)
-
-    #         poll.opened = False
-    #         poll.save()
-    #         self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        request = requests.get('http://download.geonames.org/export/dump/cities15000.zip', stream=True)
+        request.raise_for_status()
+        with ZipFile('cities15000.zip', 'r') as zfile:
+            cities = zfile.read('cities15000.txt')
+            txt_to_json(cities)
