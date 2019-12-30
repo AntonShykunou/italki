4c9c50dfb5b6b5dfb2d91b7c5c4ffa3d697e7996
Artsiom-Shlapakou <xpressmusik@mail.ru>
2019-12-30 12:33:48 +0300

    update_load_city

--- backend/locations/management/commands/load_city.py
+++ backend/locations/management/commands/load_city.py
@@ -1,19 +1,36 @@
 from django.core.management.base import BaseCommand, CommandError
 from locations.models import City
 from zipfile import ZipFile
-from itertools import groupby
+from urllib.request import urlopen
+from tempfile import TemporaryFile
+from shutil import unpack_archive
 import requests, json
 
-def txt_to_json(file):
+
+def unzip_file(url):
+    response = requests.get(url)
+    with TemporaryFile() as tfile:
+        tfile.write(response.content)
+        tfile.seek(0)
+        with ZipFile(tfile) as zfile:
+            zfile.extractall('tmp/')
+
+
+def parse_txt(file):
     cities = []
-    with open(file, encoding='utf-8') as file:
+    with open(file, encoding='utf-8') as f:
     # open('cities.json', 'w') as out_file:
-        for line in in_file:
+        for line in f:
             line = line.split('	')
-            cities.append({'city': line[1]})
-    return json.dumps({'cities': cities})
+            cities.append(line[1])
+    # return json.dumps({'cities': cities})
+    return cities
 
 
+def create_cities(obj, _list):
+    obj.objects.bulk_create(
+        [obj(name) for name in _list]
+    )
 
 class Command(BaseCommand):
     help = 'Load cities in db'
@@ -22,8 +39,17 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        request = requests.get('http://download.geonames.org/export/dump/cities15000.zip', stream=True)
-        request.raise_for_status()
-        with ZipFile('cities15000.zip', 'r') as zfile:
-            cities = zfile.read('cities15000.txt')
-            txt_to_json(cities)
+        url = 'http://download.geonames.org/export/dump/cities15000.zip'
+        temp_path = 'tmp/cities15000.txt'
+        unzip_file(url)
+        create_cities(City, parse_txt(temp_path))
+        
+        # response = requests.get('http://download.geonames.org/export/dump/cities15000.zip')
+
+        # file = TemporaryFile()
+        # file.write(response.content)
+        # fzip = ZipFile(file)
+        # fzip.extractall('tmp/')
+        # # txt_to_json('tmp/')
+        # file.close()
+        # fzip.close()
